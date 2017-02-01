dff6228616aafa5d142dcfa8c0fef6bec34d0a26
jackson-wells <welljack@oregonstate.edu>
2017-01-31 19:13:18 -0800

    syntax update

--- src/main/java/edu/oregonstate/cs361/battleship/Main.java
+++ src/main/java/edu/oregonstate/cs361/battleship/Main.java
@@ -1,5 +1,6 @@
 package edu.oregonstate.cs361.battleship;
 
+import com.google.gson.Gson;
 import spark.Request;
 import static spark.Spark.get;
 import static spark.Spark.post;
@@ -21,75 +22,141 @@ public class Main {
 
     //This function should return a new model
     private static String newModel() {
-	Gson gson = new Gson();
-        var model = { 
-		"aircraftCarrier": {
-        		"name": "AircraftCarrier",
-		        "length": 5,  
-		        "start": { "Across": 0,"Down": 0 },
-		        "end": {"Across": 0, "Down": 0}
-    			},
-		"battleship": {
-		        "name": "Battleship",
-		        "length": 4,
-		        "start": { "Across": 0,"Down": 0 },
-		        "end": {"Across": 0, "Down": 0}
-    			},
-		"cruiser": {
-		        "name": "Cruiser",
-		        "length": 3,
-		        "start": { "Across": 0,"Down": 0 },
-		        "end": {"Across": 0, "Down": 0}
-    			},
-		"destroyer": {
-		        "name": "Destroyer",
-		        "length": 2,
-		        "start": { "Across": 0,"Down": 0 },
-		        "end": {"Across": 0, "Down": 0}
-			},
-		"submarine": {
-		        "name": "Submarine",
-		        "length": 2,
-		       "start": { "Across": 0,"Down": 0 },
-		        "end": {"Across": 0, "Down": 0}
-			},
-		"computer_aircraftCarrier": {
-		        "name": "Computer_AircraftCarrier",
-		        "length": 5,
-		        "start": { "Across": 2,"Down": 2 },
-		        "end": {"Across": 2, "Down": 7}
-    			},
-		"computer_battleship": {
-		        "name": "Computer_Battleship",
-		        "length": 4,
-		        "start": { "Across": 2,"Down": 8 },
-		        "end": {"Across": 6, "Down": 8}
-			},
-		"computer_cruiser": {
-		        "name": "Computer_Cruiser",
-		        "length": 3,
-		        "start": { "Across": 4, "Down": 1 },
-		        "end": {"Across": 4, "Down": 4}
-			},
-		"computer_destroyer": {
-		        "name": "Computer_Destroyer",
-		        "length": 2,
-		        "start": { "Across": 7, "Down": 3 },
-		        "end": {"Across": 7, "Down": 5}
-    			},
-		"computer_submarine": {
-		        "name": "Computer_Submarine",
-		        "length": 2,
-		        "start": { "Across": 9, "Down": 6 },
-		        "end": {"Across": 9, "Down": 8}
-			},
-		"playerHits": [],
-		"playerMisses": [],
-		"computerHits": [],
-		"computerMisses": []
-	}
-	String jsonInString = gson.toJson(model);
-	return jsonInString;
+    	GSON gson = new Gson();
+        var BattleshipModel =
+				{
+						"aircraftCarrier":
+							{
+        						"name": "AircraftCarrier",
+		        				"length": 5,
+		        				"start":
+									{
+										"Across": 0,
+										"Down": 0 },
+		        				"end":
+									{
+										"Across": 0,
+										"Down": 0}
+							},
+						"battleship":
+							{
+		        				"name": "Battleship",
+		        				"length": 4,
+		        				"start":
+									{
+										"Across": 0,
+										"Down": 0
+									},
+		        				"end":
+									{
+										"Across": 0,
+										"Down": 0
+									}
+    						},
+						"cruiser":
+							{
+		        				"name": "Cruiser",
+		        				"length": 3,
+		        				"start":
+									{
+										"Across": 0,
+										"Down": 0 },
+		        				"end":
+									{
+										"Across": 0,
+										"Down": 0}
+    						},
+						"destroyer":
+							{
+		        				"name": "Destroyer",
+		        				"length": 2,
+		        				"start":
+									{
+										"Across": 0,
+										"Down": 0 },
+		        				"end":
+									{
+										"Across": 0,
+										"Down": 0}
+							},
+						"submarine":
+							{
+		        				"name": "Submarine",
+		        				"length": 2,
+		       					"start":
+									{
+										"Across": 0,
+										"Down": 0
+									},
+		        				"end":
+									{
+										"Across": 0,
+										"Down": 0
+									}
+							},
+						"computer_aircraftCarrier":
+							{
+								"name": "Computer_AircraftCarrier",
+								"length": 5,
+								"start":
+									{
+										"Across": 2,
+										"Down": 2
+									},
+								"end":
+									{
+										"Across": 2,
+										"Down": 7
+									}
+							},
+						"computer_battleship":
+							{
+								"name": "Computer_Battleship",
+								"length": 4,
+								"start":
+									{
+										"Across": 2,
+										"Down": 8
+									},
+								"end":
+								{
+									"Across": 6,
+									"Down": 8
+								}
+							},
+						"computer_cruiser": {
+								"name": "Computer_Cruiser",
+								"length": 3,
+								"start": { "Across": 4, "Down": 1 },
+								"end": {"Across": 4, "Down": 4}
+							},
+						"computer_destroyer": {
+								"name": "Computer_Destroyer",
+								"length": 2,
+								"start": { "Across": 7, "Down": 3 },
+								"end": {"Across": 7, "Down": 5}
+								},
+						"computer_submarine":
+							{
+								"name": "Computer_Submarine",
+								"length": 2,
+								"start":
+									{
+										"Across": 9,
+										"Down": 6
+									},
+								"end":
+									{
+										"Across": 9,
+										"Down": 8}
+									},
+						"playerHits": [],
+						"playerMisses": [],
+						"computerHits": [],
+						"computerMisses": []
+				}
+	String jsonString = gson.toJson(BattleshipModel);
+	return jsonString;
     }
 
     //This function should accept an HTTP request and deseralize it into an actual Java object.
