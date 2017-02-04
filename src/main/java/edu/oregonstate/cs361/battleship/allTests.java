96ca1ce3cb6af2983b90709c5d3ff28004b9528d
stribyw <stribyw@oregonstate.edu>
2017-02-03 17:07:14 -0800

    add test file

---
+++ src/main/java/edu/oregonstate/cs361/battleship/allTests.java
@@ -0,0 +1,31 @@
+package edu.oregonstate.cs361.battleship;
+
+import com.google.gson.Gson;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import spark.Spark;
+import spark.utils.IOUtils;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.OutputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static spark.Spark.awaitInitialization;
+
+/**
+ * Created by williamstribyjr on 2/3/17.
+ */
+class allTests {
+
+    @Test
+    public void testfireAt() {
+        TestResponse res = request("POST", "/fire/3/1");
+        assertEquals("3", col);
+        assertEquals("1", row);
+    }
+}
